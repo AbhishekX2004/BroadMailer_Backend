56960789e303f9d252c0afd0a04ef6780030c5c0
Abhishek Verma <abhishekaverma20@gmail.com>
2024-04-14 18:49:23 +0530

    Production redirects conditions added

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ const initialize = async () => {
     // billing Routes
     billingRoutes(app);
 
+    // To run while in production
+    // telling express to redirect unknown routes to react router
+    if(process.env.NODE_ENV === 'production'){
+        // express serves production assets main.js or main.css
+        app.use(express.static('client/build'));   
+        
+        // express serves index.html
+        app.get('*', (req,res) => {
+            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        })
+    }
+
     app.listen(PORT,()=>{
         console.log(`App is listening on port:${PORT}`);
     });
